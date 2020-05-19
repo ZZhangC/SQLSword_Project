4dfc1b05f3b750a744ca85a8b2a99453db4bb0e0
ZZhangC <57025975+ZZhangC@users.noreply.github.com>
2020-05-19 22:05:58 +0800

    Add files via upload

---
+++ hewindow.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'SQLSword-HugeError.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(240, 130)
+        Dialog.setMinimumSize(QtCore.QSize(240, 130))
+        Dialog.setMaximumSize(QtCore.QSize(240, 130))
+        self.gridLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 241, 131))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 2, 0, 1, 1)
+        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "ERROR"))
+        self.pushButton.setText(_translate("Dialog", "OK"))
+        self.label.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:11pt; font-weight:600;\">发生了不可名状的错误!</span></p></body></html>"))
+
--- main.py
+++ main.py
@@ -5,8 +5,26 @@
 '''
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import mwindow, ewindow, awindow, swindow, c1window, c2window, scwindow, sfwindow, sswindow
+import mwindow, ewindow, awindow, swindow, c1window, c2window, scwindow, sfwindow, sswindow, hewindow
+from requests import get
 import threading
+
+conn = False
+
+def Check():
+    t = threading.Thread(target=CheckConnect, name='1')
+    t.start()
+    t.join()
+
+def CheckConnect():
+    url = ''
+    url = MainWindow.lineEdit.text()
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.72'}
+    if url[0:7] != 'http://' or url[0:7] != 'http:\\\\' or url[0:8] != 'https://' or url[0:8] != 'https:\\\\':
+        url = 'http://' + url
+    res = get(url,headers=headers)
+    if str(res)[11:14] == '200':
+        conn = True
 
 if __name__ == '__main__':
     scanres = False
@@ -21,6 +39,7 @@ if __name__ == '__main__':
     ScanWindow = QMainWindow()
     ScanFaildWindow = QMainWindow()
     ScanSuccessWindow = QMainWindow()
+    HugeErrorWindow = QMainWindow()
 
     ui = mwindow.Ui_MainWindow()
     eui = ewindow.Ui_Dialog()
@@ -31,6 +50,7 @@ if __name__ == '__main__':
     scui = scwindow.Ui_Form()
     sfui = sfwindow.Ui_Dialog()
     ssui = sswindow.Ui_Dialog()
+    heui = hewindow.Ui_Dialog()
 
     ui.setupUi(MainWindow)
     eui.setupUi(ErrorWindow)
@@ -41,13 +61,15 @@ if __name__ == '__main__':
     scui.setupUi(ScanWindow)
     sfui.setupUi(ScanFaildWindow)
     ssui.setupUi(ScanSuccessWindow)
+    heui.setupUi(HugeErrorWindow)
 
     MainWindow.show()
 
     ui.commandLinkButton.clicked.connect(ErrorWindow.show)
     ui.commandLinkButton_2.clicked.connect(ErrorWindow.show)
     ui.actionHelp.triggered.connect(AboutWindow.show)
-    ui.pushButton_4.clicked.connect(Connect1Window.show)
+    ui.pushButton_4.clicked.connect(Check)
+    ui.pushButton_9.clicked.connect(ScanWindow.show)
     eui.pushButton.clicked.connect(ErrorWindow.close)
     sui.pushButton.clicked.connect(SuccessWindow.close)
     aui.pushButton.clicked.connect(AboutWindow.close)
@@ -55,6 +77,6 @@ if __name__ == '__main__':
     c2ui.pushButton.clicked.connect(Connect2Window.close)
     sfui.pushButton.clicked.connect(ScanFaildWindow.close)
     ssui.pushButton.clicked.connect(ScanSuccessWindow.close)
-    ui.pushButton_9.clicked.connect(ScanWindow.show)
+    heui.pushButton.clicked.connect(HugeErrorWindow.close)
 
     sys.exit(app.exec_())
